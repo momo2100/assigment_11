4eb2562e423bfdca9665a86aca7780702f7a9b77
momo2100 <momo2100@gmail.com>
2021-02-14 12:13:03 +0700

    Angular assigment 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import {FormsModule} from '@angular/forms';
+import { Lab4Component } from './lab4/lab4.component';
 
 @NgModule({
   declarations: [ 
     AppComponent,
-    HomeComponent
+    Lab4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lab4/lab4.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+selector: 'app-lab4',
+templateUrl: './lab4.component.html',
+styleUrls: ['./lab4.component.css']
+
+})
+
+export class Lab4Component {
+    num_arr:number[]=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+    constructor() {}
+
+}
